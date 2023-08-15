dab4697dbc173380fccdbc2a3f56891d54685dda
sKyoe <1552889375@qq.com>
2023-08-15 15:19:28 +0800

    配置elementplus和@别名

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+// elementPlus插件
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+// 国际化配置，语言设置为中文
+//@ts-ignore
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+app.mount("#app");
